36535f5abb05608a12b2ddf4ba370464593be5c4
Bilal Abbas <bilal.abbas@codebricks.co>
2026-06-22 15:12:46 +0500

    setting of variables in the build

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-set -e
+set -eu
+
+: "${POSTGRES_USER:?POSTGRES_USER is required}"
+: "${POSTGRES_PASSWORD:?POSTGRES_PASSWORD is required}"
+: "${POSTGRES_DB:?POSTGRES_DB is required}"
+: "${POSTGRES_HOST:?POSTGRES_HOST is required}"
+: "${POSTGRES_INTERNAL_PORT:?POSTGRES_INTERNAL_PORT is required}"
+
+export DATABASE_URL="$(node -e "const user = encodeURIComponent(process.env.POSTGRES_USER); const password = encodeURIComponent(process.env.POSTGRES_PASSWORD); const database = encodeURIComponent(process.env.POSTGRES_DB); const host = process.env.POSTGRES_HOST; const port = process.env.POSTGRES_INTERNAL_PORT; process.stdout.write(\`postgresql://\${user}:\${password}@\${host}:\${port}/\${database}?sslmode=disable\`);")"
 
 cd /app/apps/api
 
@@ -13,4 +21,3 @@ fi
 
 cd /app
 exec "$@"
-
